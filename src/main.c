3215974c8502627d7cadfbfccbc0719ca4bdc714
Mohamed Amine Amir <mamir@c3r9p10.1337.ma>
2024-09-04 10:28:27 +0100

    conflicts

--- src/main.c
+++ src/main.c
@@ -2,17 +2,5 @@
 
 int main(int ac, char **av)
 {
-<<<<<<< HEAD
-    char prompt[] = "minishell$>";
-    av = NULL;
-    if (ac == 1)
-    {
-        while(1)
-        {
-            printf("%s\n", prompt);
-            sleep(2);
-        }
-    }
-=======
->>>>>>> main
+    
 }
