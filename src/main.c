2e86de8f263183f9907abbd2d581ac50a87c4a76
Montassir Bouifraden <mobouifr@c3r9p8.1337.ma>
2024-09-04 10:18:44 +0100

    test add

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
-#include "../includes/minishell.h"
+#include "minishell.h"
+
+int ft_isspace(int c)
+{
+	if(c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == 32)
+		return (1);
+	return (0);
+}
 
 int main(int ac, char **av)
 {
     int		i;
 	int		j;
+	t_list	**lst;
 
 	j = 0;
     while (av[j] != NULL)
@@ -11,28 +19,34 @@ int main(int ac, char **av)
 		i = 0;
 		while (av[j][i] != '\0')
 		{
-			if (av[j][i] == 34 || av[j][i] == 39 || av[j][i] == ' ' || av[j][i] == '	') // 34 for (") and 39 for (').
+			if (av[j][i] == 34 || av[j][i] == 39 || ft_isspace(av[j][i])) // 34 for (") and 39 for (').
 				i++;
 			else if (av[j][i] == '|')
 			{
-				
+				ft_lstaddback(lst, ft_lstnew("|", PIPE));
 			}
 			else if (av[j][i] == '>')
 			{
+				ft_lstaddback(lst, ft_lstnew(">", INRED));
 			}
 			else if (av[j][i] == '<')
 			{
+				ft_lstaddback(lst, ft_lstnew("<", OUTRED));
 			}
-			else if (av[j][i] == '>>')
+			else if (av[j][i] == '>' && av[j][i + 1] == '>')
 			{
+				ft_lstaddback(lst, ft_lstnew(">>", APPEND));
 			}
-			else if (av[j][i] == '<<')
+			else if (av[j][i] == '<' && av[j][i] == '<')
 			{
+				ft_lstaddback(lst, ft_lstnew("|", HEREDOC));
 			}
 			else
 			{
+
 			}
 		}
 		j++;
 	}
 }
+
