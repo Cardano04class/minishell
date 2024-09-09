a1eb8ae84fee7f5bce44a2521b9c4afd7b4f96c9
Montassir Bouifraden <mobouifr@c3r10p6.1337.ma>
2024-09-09 13:22:41 +0100

    f

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,17 +1,17 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "libft.h"
 # include <fcntl.h>
 # include <limits.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdarg.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "libft.h"
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
 
-typedef enum	e_token
+typedef enum e_token
 {
 	WORD,
 	INRED,
@@ -19,20 +19,20 @@ typedef enum	e_token
 	APPEND,
 	HEREDOC,
 	PIPE
-}	t_token;
+}					t_token;
 
 typedef enum e_state
 {
-    INITIAL,
-    IN_WORD,
+	INITIAL,
+	IN_WORD,
 	IN_QUOTE,
-    IN_SPECIAL,
-    IN_WHITESPACE
-}   t_state;
+	IN_SPECIAL,
+	IN_WHITESPACE
+}					t_state;
 
 typedef struct s_list
 {
-	char 			*content;
+	char			*content;
 	t_token			type;
 	struct s_list	*next;
 	struct s_list	*prev;
@@ -40,14 +40,14 @@ typedef struct s_list
 
 typedef struct s_env
 {
-	char 		*key;
-	char 		*value;
-	struct s_env *next;
-} 					t_env;
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
 
 /*--------shell---------*/
-void 		lexer(char *str, t_list **lst);
-void 		syntax_error(t_list *list);
+void				lexer(char *str, t_list **lst);
+void				syntax_error(t_list *list);
 
 ////////////////////..LINKED LIST FUNCTIONS../////////////////////
 t_list				*ft_lstnew(char *content, t_token type);
