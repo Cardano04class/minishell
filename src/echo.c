5ac5a4e157834cff7dea3e48d02db3deb005bc35
Amine04Amir <raja_amir@outlook.fr>
2024-09-15 00:40:58 +0100

    echo syntax fix

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: mamir <mamir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 00:12:03 by mamir             #+#    #+#             */
-/*   Updated: 2024/09/15 00:36:57 by mamir            ###   ########.fr       */
+/*   Updated: 2024/09/15 00:40:17 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,65 +15,49 @@
 bool has_n_flag(char *str, int *i) {
     int j = *i;
     
-    // Skip any leading spaces
     while (str[j] == ' ')
         j++;
-
-    // Check if "-n" is present after skipping spaces
-    if (str[j] == '-' && str[j + 1] == 'n' && (str[j + 2] == ' ' || str[j + 2] == '\0')) {
-        // Move index past "-n" and any following spaces
+    if (str[j] == '-' && str[j + 1] == 'n' && (str[j + 2] == ' ' || str[j + 2] == '\0')) 
+    {
         j += 2;
         while (str[j] == ' ')
             j++;
         *i = j;
         return true;
     }
-
     return false;
 }
 
-void echo(t_list **list, char *str) {
-    int i = 4;  // Start after "echo"
+void echo(t_list **list, char *str) 
+{
+    int i = 4;  
     bool newline = true;
 
-    (void)list;  // Unused parameter to avoid warnings
-
-    // Check if the command starts with "echo"
-    if (strncmp(str, "echo", 4) == 0) {
-        // Ensure there is something after "echo"
-        if (str[i] == '\0') {
-            // Just "echo" without any additional text, print a newline
+    (void)list;  
+    if (strncmp(str, "echo", 4) == 0) 
+    {
+        if (str[i] == '\0') 
+        {
             write(1, "\n", 1);
             return;
         }
-
-        // Skip any spaces after "echo"
         while (str[i] == ' ')
             i++;
-
-        // Check for the "-n" flag
-        if (has_n_flag(str, &i)) {
-            newline = false;  // Suppress newline if "-n" is found
-        }
-
-        // Skip leading spaces after possible "-n" flag
+        if (has_n_flag(str, &i)) 
+            newline = false;  
         while (str[i] == ' ')
             i++;
-
-        // If there is still no content after "echo" or "-n", return without printing anything
-        if (str[i] == '\0') {
+        if (str[i] == '\0') 
+        {
             if (newline)
                 write(1, "\n", 1);
             return;
         }
-
-        // Print the rest of the string
-        while (str[i]) {
+        while (str[i]) 
+        {
             write(1, &str[i], 1);
             i++;
         }
-
-        // Add newline if the "-n" flag was not provided
         if (newline)
             write(1, "\n", 1);
     }
