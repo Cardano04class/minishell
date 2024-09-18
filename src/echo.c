d3539ef00f9be08035f83d44d3b9d0df5c50301f
Amine04Amir <raja_amir@outlook.fr>
2024-09-18 18:13:49 +0100

    echo dial 3bar

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: mamir <mamir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 00:12:03 by mamir             #+#    #+#             */
-/*   Updated: 2024/09/17 17:08:17 by mamir            ###   ########.fr       */
+/*   Updated: 2024/09/18 18:12:42 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,49 @@
 
 int echo(char **str)
 {
-    int flag = false;
-    int lock = false;
+    int flag = false;  
+    int lock = false;  
     int i = 0;
-
+    bool is_first_argument = true; 
     while (str[i])
     {
-        if (str[i][0] == '-' && !lock)
+        if (is_first_argument && str[i][0] == '-')
         {
             int j = 1;
+            bool all_n = true;
             while (str[i][j])
             {
                 if (str[i][j] != 'n')
                 {
-                    lock = false;
+                    all_n = false;
                     break;
                 }
                 j++;
             }
-        }
-        else
-        {
-            int j = 0;
-            while (str[i][j])
+            if (all_n)
             {
-                write(1, &str[i][j], 1);
-                j++;
+                lock = true;
+                i++;
+                continue;
             }
-            if (str[i + 1]) 
-                write(1, " ", 1);
-            flag = true;
         }
+        is_first_argument = false;
+        int j = 0;
+        while (str[i][j])
+        {
+            write(1, &str[i][j], 1); 
+            j++;
+        }
+
+        if (str[i + 1])
+            write(1, " ", 1);
+
+        flag = true;
         i++;
     }
-    if (lock && !flag)
+    if (!lock && flag)
         write(1, "\n", 1);
     
     return 0;
 }
+
