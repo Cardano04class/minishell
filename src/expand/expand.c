b44250819fd12ad0cde4fe72a3c45c72d2b6796b
Mohamed Amine Amir <mamir@c3r9p10.1337.ma>
2024-11-05 22:42:39 +0100

    need to fix whitespace in multiple quotes

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -6,81 +6,102 @@
 /*   By: mamir <mamir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 16:26:11 by mamir             #+#    #+#             */
-/*   Updated: 2024/11/05 21:50:43 by mamir            ###   ########.fr       */
+/*   Updated: 2024/11/05 22:41:32 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+#define BUFFER_SIZE 1024
+
+
+// Main function to expand variables and handle quotes in input
 char *expand_variables(t_env *env, char *line)
 {
-    char *result = malloc(1024);
-    if (!result) return NULL;
+    char *result;
     int result_idx = 0;
     int i = 0;
     int in_single_quote = 0;
     int in_double_quote = 0;
 
+    result = malloc(BUFFER_SIZE);  // Allocate buffer size
+    if (!result)
+        return NULL;
+
     while (line[i])
     {
+        // Handle single quotes
         if (line[i] == '\'' && !in_double_quote)
         {
             in_single_quote = !in_single_quote;
-            i++;
-            continue; // Skip the single quote
+            result[result_idx++] = line[i++];
+            continue;
         }
+
+        // Handle double quotes
         if (line[i] == '\"' && !in_single_quote)
         {
             in_double_quote = !in_double_quote;
-            i++;
-            continue; // Skip the double quote
+            result[result_idx++] = line[i++];
+            continue;
         }
+
+        // Handle variable expansion only if not in single quote
         if (line[i] == '$' && !in_single_quote)
         {
             i++;
             char var_name[100];
             int var_idx = 0;
             
+            // Capture variable name
             while (line[i] && (ft_isalnum(line[i]) || line[i] == '_'))
             {
                 var_name[var_idx++] = line[i++];
             }
             var_name[var_idx] = '\0';
 
+            // Get variable value
             if (var_idx > 0)
             {
                 char *var_value = get_env(env, var_name);
                 if (var_value)
                 {
+                    // Copy value into result
                     strcpy(&result[result_idx], var_value);
                     result_idx += strlen(var_value);
                 }
-                continue;
             }
             continue;
         }
+
+        // Normal character addition
         result[result_idx++] = line[i++];
     }
 
-    result[result_idx] = '\0';
+    result[result_idx] = '\0';  // Null-terminate result
     return result;
 }
 
-
+// Function to handle and remove quotes from a string
 char *handle_quotes(char *str)
 {
-    if (!str)
-        return NULL;
-    char *result = malloc(strlen(str) + 1);
-    if (!result)
-        return NULL;
+    char *result;
     int i = 0;
     int j = 0;
     int in_single_quote = 0;
     int in_double_quote = 0;
 
+    if (!str)
+        return NULL;
+
+    result = malloc(strlen(str) + 1);
+    if (!result)
+        return NULL;
+
+    // Process each character of the string
     while (str[i])
     {
+        // Toggle quotes
         if (str[i] == '\'' && !in_double_quote)
         {
             in_single_quote = !in_single_quote;
@@ -93,9 +114,14 @@ char *handle_quotes(char *str)
             i++;
             continue;
         }
+
+        // Append character to result
         result[j++] = str[i++];
     }
-    result[j] = '\0';
+
+    result[j] = '\0';  // Null-terminate result
+
+    // Return empty string if no content
     if (j == 0)
     {
         free(result);
@@ -105,6 +131,9 @@ char *handle_quotes(char *str)
     return result;
 }
 
+
+
+// Function to expand commands in the mini shell
 void expand(t_env *env)
 {
     int i = 0;
--- src/expand/expand_utils.c
+++ src/expand/expand_utils.c
@@ -1,14 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   expand_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mamir <mamir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 23:10:50 by codespace         #+#    #+#             */
-/*   Updated: 2024/10/27 23:11:01 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/05 22:22:13 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// Function to shift commands in the command array after removing an entry
+void shift_commands(char **cmd, int index)
+{
+    int j = index;
+
+    free(cmd[index]);
+    while (cmd[j + 1])
+    {
+        cmd[j] = cmd[j + 1];
+        j++;
+    }
+    cmd[j] = NULL;
+}
 
