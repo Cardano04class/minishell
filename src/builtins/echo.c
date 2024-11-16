c28d6130601956c145a0aa4221aad8c520e511f7
Mohamed Amine Amir <mamir@c3r9p7.1337.ma>
2024-11-16 14:41:28 +0100

    Norm echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,58 +6,64 @@
 /*   By: mamir <mamir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 00:12:03 by mamir             #+#    #+#             */
-/*   Updated: 2024/11/13 11:00:57 by mamir            ###   ########.fr       */
+/*   Updated: 2024/11/16 14:40:52 by mamir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int echo(char **str)
+bool	is_n_option(char *str)
 {
-    int i = 1;
-    int j;
-    bool n_option = false;
+	int	j;
 
-    // If no arguments, just print newline and return
-    if (str[1] == NULL)
-    {
-        write(1, "\n", 1);
-        return (0);
-    }
-    // Process -n options
-    while (str[i] && str[i][0] == '-' && str[i][1] == 'n')
-    {
-        j = 2;
-        while (str[i][j] == 'n')
-            j++;
-        if (str[i][j] == '\0')  // Only if the argument is all 'n's
-        {
-            n_option = true;
-            i++;
-        }
-        else
-            break;
-    }
-    // Print arguments
-    while (str[i])
-    {
-        j = 0;
-        while (str[i][j])
-        {
-            write(1, &str[i][j], 1);
-            j++;
-        }
-        if (str[i + 1])  // Print space only if not the last argument
-            write(1, " ", 1);
-        i++;
-    }
-
-    // Print newline if -n was not specified
-    if (!n_option)
-        write(1, "\n", 1);
-
-    return (0);
+	if (str[0] != '-' || str[1] != 'n')
+		return (false);
+	j = 2;
+	while (str[j] == 'n')
+		j++;
+	if (str[j] == '\0')
+		return (true);
+	return (false);
 }
 
+int	print_arguments(char **str, int i)
+{
+	int	j;
 
+	while (str[i])
+	{
+		j = 0;
+		while (str[i][j])
+		{
+			write(1, &str[i][j], 1);
+			j++;
+		}
+		if (str[i + 1])
+			write(1, " ", 1);
+		i++;
+	}
+	return (i);
+}
 
+int	echo(char **str)
+{
+	int		i;
+	bool	n_option;
+
+	i = 1;
+	n_option = false;
+	if (str[1] == NULL)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (str[i] && is_n_option(str[i]))
+	{
+		n_option = true;
+		i++;
+	}
+	print_arguments(str, i);
+	if (!n_option)
+		write(1, "\n", 1);
+	return (0);
+}
